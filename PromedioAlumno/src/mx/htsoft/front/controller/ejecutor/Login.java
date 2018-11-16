8ad13181440997a24fd8f79267f0105629f7f011
BrandonOmarPadillaLopez <44876693+BrandonOmarPadillaLopez@users.noreply.github.com>
2018-11-16 12:30:02 -0600

    Creación de nuestro Main

---
+++ PromedioAlumno/src/mx/htsoft/front/controller/ejecutor/Login.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.htsoft.front.controller.ejecutor;
+
+/**
+ *
+ * @author omarp
+ */
+public class Login {
+ 
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
